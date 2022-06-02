75abaa15e9297302bc7ba75803236d8f6563a6b3
minjman2659 <minjman2659@gmail.com>
2022-06-02 23:42:23 +0900

    [2-TYPES-PROJECTS] calculator 함수 구현

---
+++ 2-TYPES-PROJECTS/calculator.ts
@@ -0,0 +1,27 @@
+/**
+ * Let's make a calculator 🧮
+ */
+type Func = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+
+function calculate(func: Func, num1: number, num2: number): number {
+  switch (func) {
+    case 'add':
+      return num1 + num2;
+    case 'substract':
+      return num1 - num2;
+    case 'multiply':
+      return num1 * num2;
+    case 'divide':
+      return num1 / num2;
+    case 'remainder':
+      return num1 % num2;
+    default:
+      throw new Error('unknown function');
+  }
+}
+
+console.log(calculate('add', 1, 3)); // 4
+console.log(calculate('substract', 3, 1)); // 2
+console.log(calculate('multiply', 4, 2)); // 8
+console.log(calculate('divide', 4, 2)); // 2
+console.log(calculate('remainder', 5, 2)); // 1
