61794396d73fb3a84edd5d02c779b158e88e0cd4
minjman2659 <minjman2659@gmail.com>
2022-06-03 20:14:19 +0900

    [2-TYPES-PROJECTS] game 함수 구현

--- 2-TYPES-PROJECTS/game.ts
+++ 2-TYPES-PROJECTS/game.ts
@@ -1,6 +1,27 @@
 /**
  * Let's make a game 🕹
  */
+const position = { x: 0, y: 0 };
+
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+function move(direction: Direction): void {
+  switch (direction) {
+    case 'up':
+      position.y++;
+      return;
+    case 'down':
+      position.y--;
+      return;
+    case 'left':
+      position.x--;
+      return;
+    case 'right':
+      position.x++;
+      return;
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
