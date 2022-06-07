b92326c50b9778e98782273dddc0c0b89d8a5dcf
minjman2659 <minjman2659@gmail.com>
2022-06-07 20:28:04 +0900

    [4-OOP-PROJECT] stack find 메소드 구현

--- 4-OOP-PROJECT/stack.ts
+++ 4-OOP-PROJECT/stack.ts
@@ -5,6 +5,7 @@
     readonly size: number;
     push(vlaue: string): void;
     pop(): string;
+    find(index: number): string;
   }
 
   type Node = {
@@ -47,6 +48,27 @@
       this._size--;
       return deletedNode.value;
     }
+
+    find(index: number): string {
+      if (!this.head) {
+        throw new Error('Stack is Empty!');
+      }
+      if (index >= this._size) {
+        throw new Error('Index is Over the Size of Stack!');
+      }
+      if (index < 0) {
+        throw new Error('Index must Over the Zero');
+      }
+      if (index === this.head.index) {
+        return this.head.value;
+      } else {
+        let findNode = this.head.beforeNode;
+        while (findNode?.index !== index) {
+          findNode = findNode?.beforeNode;
+        }
+        return findNode.value;
+      }
+    }
   }
 
   const stack = new Stack(5);
@@ -58,6 +80,9 @@
   stack.push('test3');
   console.log(stack);
   console.log(stack.size);
+
+  const found = stack.find(0);
+  console.log(found);
 
   const popped = stack.pop();
   console.log(popped);
