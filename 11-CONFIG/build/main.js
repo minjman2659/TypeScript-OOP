9988dcc3277c609e8e712b136f445b5f97fbe6c7
minjman2659 <minjman2659@gmail.com>
2022-06-17 00:17:27 +0900

    [11-CONFIG] config.ts 진행

---
+++ 11-CONFIG/build/main.js
@@ -0,0 +1,13 @@
+"use strict";
+class Car {
+    constructor() {
+        this.engine = 0;
+    }
+    move() {
+        const engine = this.engine + 1;
+        console.log(engine);
+    }
+}
+const car = new Car();
+car.move();
+//# sourceMappingURL=main.js.map
